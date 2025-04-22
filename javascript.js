ea34e8a741c621069586f0ff252719d3f162320a
kennythedesigner <kennedicosma22@outlook.com>
2025-04-22 18:48:28 -0400

    first lines of javascript basics

---
+++ javascript.js
@@ -0,0 +1,21 @@
+let name = "John"
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+console.log(23+77+0+9+22+65);
+
+let a = 25;
+
+console.log(a);
+ let b = 7 * a;
+console.log(b);
+``
+ /////
+
+const  max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
+
